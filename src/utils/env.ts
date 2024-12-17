43e0c1e66f7d0fb31230f5e9733949d772c43bb3
ahmadstiff <royanuahmad313@gmail.com>
2024-12-17 10:38:44 +0700

    add url

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,1 +1,1 @@
-export const DATABASE_URL = "mongodb+srv://royanuahmad313:<db_password>@cluster0.uf0my.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+export const DATABASE_URL = "mongodb+srv://db-acara:admin#1234@ticket-course.d3gbd.mongodb.net/?retryWrites=true&w=majority&appName=ticket-course";
